7975472c55305c4541070fdfb21e3d82acabdd6d
eduard <playtriter3@gmail.com>
2024-05-31 08:49:05 +0200

    conjugate gradient

--- include/EigenSolver.h
+++ include/EigenSolver.h
@@ -16,6 +16,17 @@ namespace EigenSolver
             result = solver.solve(vectorB);
         }
     };
+
+    struct CG 
+    {
+        template<typename TA, typename TB, typename TR>
+        static void solve(TA& matrixA, TB& vectorB, TR& result)
+        {
+            Eigen::ConjugateGradient<Eigen::SparseMatrix<double>> solver;
+            solver.compute(matrixA);
+            result = solver.solve(vectorB);
+        }
+    };
 };
 
 #endif
